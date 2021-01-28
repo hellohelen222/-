85a5a005a5fc332a5399f904c29640d3e732e3d0
hellohelen222 <71634382+hellohelen222@users.noreply.github.com>
2021-01-28 16:08:55 +0800

    Update order8.php

--- Seckill/public/endmiaosha/order8.php
+++ Seckill/public/endmiaosha/order8.php
@@ -404,8 +404,11 @@ if($goodsorder){
             exit;
         }
         var_dump(json_decode($line,true));
+    
         
-        
+    
+        //查询结果    
+        /*
          $status=0;
         ignore_user_abort(); //即使Client断开(如关掉浏览器)，PHP脚本也可以继续执行.  
         set_time_limit(0); // 执行时间为无限制，php默认执行时间是30秒，可以让程序无限制的执行下去  
@@ -427,6 +430,7 @@ if($goodsorder){
             echo "下单成功";
             exit();
         }
+        */
         
         
         exit;
